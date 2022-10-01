0cacc2b6bca6fb449eda15b353ad7407ca05367e
Dov Kruger <Dov.Kruger@stevens.edu>
2022-10-01 17:22:02 -0400

    added example 1, function f needs to be implemented

---
+++ example1.cc
@@ -0,0 +1,15 @@
+/*
+  Hacktoberfest example
+
+  Suppose we have some code issues to be solved by people out there on the internet.
+  Here is an example: an unfinished function f()
+
+*/
+void f() {
+  // TODO: implement this, it should print hello!
+}
+
+int main() {
+  f();  
+
+}
